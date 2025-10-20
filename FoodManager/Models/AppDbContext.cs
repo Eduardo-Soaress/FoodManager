e1bdbb3cb5cb5f8bc978316b75f63c708843fef2
Eduardo Soares <eduardosoares-1@hotmail.com>
2025-10-20 03:20:40 -0300

    Configuração inicial, Login/Logout e CRUD Usuários

---
+++ FoodManager/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodManager.Models
+{
+    //Classe responsável por fazer a configuração do Entity Framework
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    //Toda nova entidade que for criada, deve ser adicionada aqui
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ FoodManager/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodManager.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        [Required(ErrorMessage = "É obrigatório informar o ID!")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="É obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar o email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar o perfil")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Administrador,
+        Usuário        
+    }
+}
